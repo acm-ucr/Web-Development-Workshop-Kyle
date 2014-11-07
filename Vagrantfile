2c1f32adb9bfa6e269616cb6a29da97ee4717ccf
omoman <mannysanchez998@gmail.com>
2014-11-07 14:37:07 -0800

    Added GUI and apache/host config
    Added Gui setting in vagrantfile and also added GNOME and X to the list of packages to be installed in run.sh. Also added hostnames student.dev and www.student.dev to host file.

--- Vagrantfile
+++ Vagrantfile
@@ -8,15 +8,15 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # We will be using ubuntu 14.04 64-bit
   config.vm.box = "ubuntu/trusty64"
 
-  # Port 8080 is to be used for the Apache HTTP server
-  config.vm.network "forwarded_port", guest: 8080, host: 8080
+  # Run our install script
+  config.vm.provision "shell", path: "etc/install/run.sh"
 
   # Virtualbox specific settings
   config.vm.provider "virtualbox" do |vb|
     # Allocate 2048 or 2GB of memory for virtual machine  
     vb.customize ["modifyvm", :id, "--memory", "2048"]
+    # Start VM with a GUI
+    vb.gui = true
   end
 
-  # Run our install script
-  config.vm.provision "shell", path: "etc/install/run.sh"
 end
--- etc/install/run.sh
+++ etc/install/run.sh
@@ -9,6 +9,11 @@ echo 'Installing git...'
 sudo apt-get install -y git
 echo 'Done installing git'
 
+# Install GNOME GUI
+echo 'Installing GNOME...'
+sudo apt-get install -y xorg gnome-core gnome-system-tools gnome-app-install
+echo 'Done installing GNOME'
+
 # Configure mysql package with root password so the installer
 # doesn't prompt for one during installation
 echo 'Configuring mysql package with root password...'
@@ -24,11 +29,52 @@ echo 'Done installing Apache, MySql, and PHP'
 # Set up Student database for application
 echo 'Setting up Student db...'
 mysqladmin --user root --password=acmrocks create Student
+echo 'Done setting up Student db'
 
 # Install PHP extensions
 echo 'Installing some PHP extensions...'
 sudo apt-get install -y php5-curl
 echo 'Done installing PHP extensions'
+
+# Set up apache server to serve /vagrant/web
+echo 'Setting up apache server to serve /vagrant/web...'
+
+# Create virtual host for /vagrant/web
+sudo cat <<EOF > /etc/apache2/sites-available/student.dev.conf
+<VirtualHost *:80>
+DocumentRoot /vagrant/web
+ServerName student.dev
+ServerAlias www.student.dev
+ErrorLog ${APACHE_LOG_DIR}/student.dev.error.log
+</VirtualHost>
+EOF
+
+# Set permissions for /vagrant/web
+sudo cat <<EOF >> /etc/apache2/apache2.conf
+<Directory "/vagrant/web">
+Options Indexes FollowSymLinks
+AllowOverride All
+Require all granted
+</Directory>
+EOF
+
+chmod -R 755 /vagrant/web
+# Disable default host
+sudo a2dissite 000-default.conf
+# Enable new host
+sudo a2ensite student.dev.conf
+# Restart Apache
+sudo service apache2 restart
+
+echo 'Done setting up apache server'
+
+# Set www.student.dev in host file
+echo 'Setting up host file...'
+sudo echo '127.0.0.1 www.student.dev' >> /etc/hosts
+sudo echo '127.0.0.1 student.dev' >> /etc/hosts
+echo 'Done setting up host file'
+
+# 
 
 # Install composer
 echo 'Installing Composer...'
